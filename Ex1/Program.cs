709adf77ecd9af422000204c6f65115daaa1ca0d
Александр Романов <derdness@yandex.ru>
2024-01-01 23:29:56 +0700

    Initial commit

---
+++ Ex1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 1: Задайте значения M и N. 
+Напишите программу, которая выведет все 
+натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.
+*/
+
+Random rng = new Random();
+int m = rng.Next(51);
+int n = rng.Next(51,101);
+
+///<summary>
+///Выводит все числа в (закрытом) промежутке от from до to
+///в строку через пробелы.
+///Значение начала промежутка должно быть меньше
+///значения конца интервала.
+///</summary>
+/// <param name="from">Начало интервала.</param>
+/// <param name="num">Конец интервала.</param>
+void RecursiveCount(int from, int to)
+{
+    if(from<to)
+    {
+        Console.Write($"{from} ");
+        RecursiveCount(++from, to);
+    }
+    else
+    {
+        Console.WriteLine(to);
+    }
+}
